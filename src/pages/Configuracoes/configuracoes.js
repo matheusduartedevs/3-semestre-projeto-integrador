42deab129842dea37687773c47021a5353552ecc
unknown <matheus.duartesilva42@gmail.com>
2024-04-08 18:33:32 -0300

    implementação das notificações na aba de configurações

--- src/pages/Configuracoes/configuracoes.js
+++ src/pages/Configuracoes/configuracoes.js
@@ -12,3 +12,25 @@ acessibilidadeLink.addEventListener('click', () => {
     perfilCampos.style.display = 'none';
     acessibilidadeCampos.style.display = 'block';
 });
+
+document.getElementById('profile-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    if ('Notification' in window) {
+        Notification.requestPermission().then(function (permission) {
+            if (permission === 'granted') {
+                new Notification('Configurações salvas!')
+            }
+        })
+    }
+})
+
+document.getElementById('accessibility-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    if ('Notification' in window) {
+        Notification.requestPermission().then(function (permission) {
+            if (permission === 'granted') {
+                new Notification('Configurações salvas!')
+            }
+        })
+    }
+})
