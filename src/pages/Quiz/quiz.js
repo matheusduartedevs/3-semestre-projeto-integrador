198053beb54a4fb1cd66b6689b81ebf979146abf
unknown <matheus.duartesilva42@gmail.com>
2024-04-10 15:57:38 -0300

    fix: ajuste na função de selecionar resposta

--- src/pages/Quiz/quiz.js
+++ src/pages/Quiz/quiz.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const respostas = document.querySelectorAll('.resposta-item');
     respostas.forEach((resposta) => {
         resposta.addEventListener('click', () => {
+            const isSelected = resposta.classList.contains('selected')
+
             respostas.forEach((element) => element.classList.remove('selected'));
-            resposta.classList.add('selected');
+
+            if (!isSelected) {
+                resposta.classList.add('selected');
+            }
         });
     });
 });
