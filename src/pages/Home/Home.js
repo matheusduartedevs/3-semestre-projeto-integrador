014c6ded26e2f62553fdf77e72834c95b1322489
unknown <matheus.duartesilva42@gmail.com>
2024-05-21 19:51:26 -0300

    feat: implementação da função de alterar para modo escuro

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,21 @@
+const modoEscuroIcon = document.getElementById('modoescuro') // Icon do modo escuro, não tirar
+const header = document.querySelector('header') // Elemento que será manipulado, nesse caso pegando pelo elemento HTML header
+const areaConhecimento = document.getElementById('ac1') // Puxando pelo id
+
+// Verifica se o modo escuro está ativado no sessionStorage
+const isModoEscuroAtivado = sessionStorage.getItem('modoEscuro') === 'true';
+
+// Define o estado inicial com base no sessionStorage
+if (isModoEscuroAtivado) {
+    header.classList.add('dark-mode');
+    areaConhecimento.classList.add('dark-mode');
+}
+
+modoEscuroIcon.addEventListener('click', () => {
+    header.classList.toggle('dark-mode');
+    areaConhecimento.classList.toggle('dark-mode')
+
+    // Salva o estado no sessionStorage
+    sessionStorage.setItem('modoEscuro', header.classList.contains('dark-mode'));
+    sessionStorage.setItem('modoEscuro', areaConhecimento.classList.contains('dark-mode'));
+});
