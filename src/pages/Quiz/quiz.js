ab51b5ee13d2c803765784687fa80c54f7d6bacb
unknown <matheus.duartesilva42@gmail.com>
2024-05-22 16:15:56 -0300

    fi

--- src/pages/Quiz/quiz.js
+++ src/pages/Quiz/quiz.js
@@ -40,4 +40,4 @@ function verificarSaida() {
     if (confirm("Se você sair, perderá todo o progresso. Deseja continuar?")) {
         window.location.href = "../Quizes/quizes.html";
     }
-    
+}
