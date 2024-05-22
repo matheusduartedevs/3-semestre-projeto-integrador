db8b524dfc07b6a6b122df4cabffbdf351e59ea9
unknown <matheus.duartesilva42@gmail.com>
2024-05-22 16:10:51 -0300

    feat: implementação dos inputs, controle de respostas e saída, link entre os quizes

--- src/pages/Quiz/quiz.js
+++ src/pages/Quiz/quiz.js
@@ -1,24 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const respostas = document.querySelectorAll('.resposta-item');
-    respostas.forEach((resposta) => {
-        resposta.addEventListener('click', () => {
-            const isSelected = resposta.classList.contains('selected')
+    const blocosPergunta = document.querySelectorAll('.area-pergunta');
 
-            respostas.forEach((element) => element.classList.remove('selected'));
+    blocosPergunta.forEach((bloco) => {
+        const respostas = bloco.querySelectorAll('.resposta-item');
 
-            if (!isSelected) {
-                resposta.classList.add('selected');
-            }
+        respostas.forEach((resposta) => {
+            resposta.addEventListener('click', () => {
+                const isSelected = resposta.checked;
+
+                respostas.forEach((element) => {
+                    if (element !== resposta) {
+                        element.checked = false;
+                    }
+                });
+            });
         });
     });
 });
 
 function verificarResposta() {
-    const respostaSelecionada = document.querySelector('.resposta-item.selected');
-    if (respostaSelecionada) {
-        alert(`Você selecionou: ${respostaSelecionada.textContent}`);
+    const blocosPergunta = document.querySelectorAll('.area-pergunta');
+    let todasAsRespostasSelecionadas = true;
+
+    blocosPergunta.forEach((bloco) => {
+        const respostasSelecionadas = bloco.querySelectorAll('.resposta-item:checked');
+        if (respostasSelecionadas.length === 0) {
+            todasAsRespostasSelecionadas = false;
+        }
+    });
+
+    if (todasAsRespostasSelecionadas) {
+        alert('Finalizado.');
     } else {
-        alert('Por favor, selecione uma resposta antes de avançar.');
+        alert('Por favor, selecione uma resposta em cada bloco antes de avançar.');
     }
 }
 
@@ -26,4 +40,4 @@ function verificarSaida() {
     if (confirm("Se você sair, perderá todo o progresso. Deseja continuar?")) {
         window.location.href = "../Quizes/quizes.html";
     }
-}
+    
